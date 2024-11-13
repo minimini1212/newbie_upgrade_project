bb569f61ae3cbe30502ca2208863518973d62986
minimini1212 <zjonyv12@naver.com>
2024-11-14 00:53:47 +0900

    (feature/post) -feat: create: post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, HttpStatus } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { BearerTokenGuard } from 'src/auth/guard/bearer.guard';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  @UseGuards(BearerTokenGuard)
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postService.create(createPostDto);
-  }
+  async create(@Body() createPostDto: CreatePostDto, @Req() req) {
+    const userId = req.userId;
+    console.log(userId)
+
+    const newPost = await this.postService.create(createPostDto, +userId);
+
+    return {
+        statusCode: HttpStatus.CREATED,
+        message: 'ok',
+        newPost,
+    };
+}
 
   @Get()
   findAll() {
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
-  }
+    constructor(
+        @InjectRepository(Post)
+        private readonly postRepository: Repository<Post>,
+    ) {}
 
-  findAll() {
-    return `This action returns all post`;
-  }
+    // 게시글 생성
+    async create(createPostDto: CreatePostDto, userId: number) {
+        const { title, content } = createPostDto;
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
+        const post = await this.postRepository.save({
+            title,
+            content,
+            userId,
+        });
+        return post;
+    }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
+    findAll() {
+        return `This action returns all post`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} post`;
+    }
+
+    update(id: number, updatePostDto: UpdatePostDto) {
+        return `This action updates a #${id} post`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} post`;
+    }
 }
